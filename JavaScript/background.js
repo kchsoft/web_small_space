6dbfdf1e0c72267d59b0e6f5e91b2981f3752025
Chang Hyeon Kim <53136613+kchsoft@users.noreply.github.com>
2022-01-16 18:32:27 +0900

    Adding picture

--- JavaScript/background.js
+++ JavaScript/background.js
@@ -1,4 +1,4 @@
-const images = ["0", "1"];
+const images = ["0", "1", "2", "3", "4"];
 const chosenimage = images[Math.floor(Math.random() * images.length)];
 document.body.classList.add(`background_${chosenimage}`);
 
